4ad705ce40d366b2b385fca922dc3f6e9f669fc0
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-03-16 17:20:53 +0100

    starting the banner

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "../css/Home.css";
-
+import Banner from "../components/Banner";
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 }
 
 export default Home;
