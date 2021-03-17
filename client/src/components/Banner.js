d328d3eeaad0840bd1128909632812394c38096f
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-03-17 15:03:51 +0100

    date picker done

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../css/Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 function Banner() {
   const [showSearch, setShowSearch] = useState(false);
@@ -8,13 +9,13 @@ function Banner() {
   return (
     <div className="banner">
       <div className="banner__search">
-        {showSearch && <h1>Show date picker</h1>}
+        {showSearch && <Search />}
         <Button
           onClick={() => setShowSearch(!showSearch)}
           className="banner__searchButton"
           variant="outlined"
         >
-          Search Dates
+          {showSearch ? "Hide" : "Search Dates"}
         </Button>
       </div>
 
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../css/Home.css";
 import Banner from "../components/Banner";
+import Card from "../components/Card";
+
 function Home() {
   return (
     <div className="home">
       <Banner />
+
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
