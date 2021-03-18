746df67283eb150203d5ba38eb2fbb688043adfd
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-03-18 17:10:15 +0100

    app done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Header from "./components/Header";
 import Footer from "./views/Footer";
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,16 +4,18 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import LanguageIcon from "@material-ui/icons/Language";
 import { Avatar } from "@material-ui/core";
 import "../css/Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__icon"
-        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-        alt=""
-      />
-
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt=""
+        />
+      </Link>
       <div className="header__center">
         <input type="text" />
         <SearchIcon />
