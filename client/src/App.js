24cec30ad7819cd87529527582aafd2866eb677e
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-03-18 15:42:09 +0100

    fin du footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./views/Home";
 import Header from "./components/Header";
+import Footer from "./views/Footer";
+import SearchPage from "./views/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
 
-      {/* Banner */}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          {/* Mettre sa route initial a la fin sinon ca affiche celui ci par default */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-      {/* Cards */}
-
-      {/* Footer */}
+        <Footer />
+      </Router>
     </div>
   );
 }
